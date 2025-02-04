dad4b29068a7d6199b721c4adbdda3a2528d44c7
Altay Hodoglugil <altay.hodo@gmail.com>
2025-02-03 23:43:37 -0800

    Add waiver wire and media queries for mobile

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,10 +4,11 @@ function HomePage() {
   return (
     <div className="home-page">
       <div className="info">
-        🐻 💻 Im a sophomore studying at UC Berkeley, majoring in Computer
-        Science & Operations Research, with a minor in Data Science. I aspire to
-        work as a software engineer or data scientist, and I have a passion for
-        the fields of technology, mathematics, and building things with code.
+        🐻 💻 Hello there! Im a sophomore studying at UC Berkeley, majoring in
+        Computer Science & Operations Research, with a minor in Data Science. I
+        aspire to work as a software engineer or data scientist, and I have a
+        passion for the fields of technology, mathematics, and building things
+        with code.
       </div>
       <div className="info">
         🏀 🏋️ In my free time, you’ll probably catch me playing basketball,
@@ -19,7 +20,7 @@ function HomePage() {
       <div>
         <h2 className="subtitle">Projects</h2>
         <div className="projects-container">
-        <table className="projects-table">
+          <table className="projects-table">
             <thead>
               <tr>
                 <th>Name</th>
@@ -29,9 +30,42 @@ function HomePage() {
             </thead>
             <tbody>
               <tr>
-                <td><a href="https://resumai-pi.vercel.app/" target="_blank" rel="noopener noreferrer">ResumAI</a></td>
-                <td><img src='/ResumAI.webp' alt="Project 1" className="project-image" /></td>
-                <td>An AI application that gives students tailored feedback on improving their resume.</td>
+                <td>
+                  <a
+                    href="https://resumai-pi.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    ResumAI
+                  </a>
+                </td>
+                <td>
+                  <img
+                    src="/ResumAI.webp"
+                    alt="Project 1 Image"
+                    className="project-image"
+                  />
+                </td>
+                <td>
+                  An AI application that gives students tailored feedback on
+                  improving their resume.
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a href="">Waiver Wire</a>
+                </td>
+                <td>
+                  <img
+                    src="/lebron.webp"
+                    alt="Project 2 Image"
+                    className="project-image"
+                  />
+                </td>
+                <td>
+                  A fantasy basketball project that forecasts player statistics
+                  and recommends players to add to your fantasy team.
+                </td>
               </tr>
             </tbody>
           </table>
